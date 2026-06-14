2661f432b86ceabe23d4d238a10fe1c987d6d8ed
Go8055 <43295772+Go8055@users.noreply.github.com>
2026-06-14 17:25:57 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Welcome to TopSMM Bot 🚀")
+
+bot.infinity_polling()
